b63f5b45f9a0048c774b8ada87c494e3c795c4c1
tiberiufrat <tiberiu.fratila@gmail.com>
2020-06-30 20:06:11 +0300

    add class game

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,21 @@
-dictionary = File.open("5desk.txt")
-lines = dictionary.read.split
-word = lines.select {|line| line.length > 5 && line.length < 12}.sample
+class HangmanGame
+  attr_accessor :guess, :word_letters, :incorrect_guesses_left, :wrong_guesses
+
+  def initialize
+    dictionary = File.open("5desk.txt")
+    @word = dictionary.read.split.select {|line| line.length > 5 && line.length < 12}.sample
+    @word_letters = word.split("")
+    @incorrect_guesses_left = 5
+    @guess = Array.new(word.length)
+    @wrong_guesses = []
+  end
+
+  def guess
+    
+  end
+
+  protected
+  def word
+    @word
+  end
+end
